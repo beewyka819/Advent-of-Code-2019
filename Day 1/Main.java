d1d5ef0d1fb452dac3257d4641fb922190f611c7
beewyka819 <psullivang7@gmail.com>
2019-12-01 23:08:48 -0500

    Add files via upload

---
+++ Day 1/Main.java
@@ -0,0 +1,36 @@
+package com.webs.beewyka819.one;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> modules = new ArrayList<Integer>();
+
+        try(InputStream in = Main.class.getResourceAsStream("/res/modules.txt");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+                String line;
+                while((line = reader.readLine()) != null) {
+                    modules.add(Integer.parseInt(line));
+                }
+                reader.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        int sum = 0;
+        for(int module : modules) {
+            int additionalFuel = module / 3 - 2;
+            while(additionalFuel > 0) {
+                sum += additionalFuel;
+                additionalFuel = additionalFuel / 3 - 2;
+            }
+        }
+        System.out.println(sum);
+    }
+}
